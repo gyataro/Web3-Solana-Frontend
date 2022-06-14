ef0e746aedb0019a8c4e444a851f51bb286a65a3
Xiwen Teoh <teoh6g@gmail.com>
2022-06-14 17:20:12 +0800

    Added Solana wallet interface

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,53 @@
 import React, { useMemo } from "react";
+// WalletAdapterNetwork: all available networks
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+// WalletModalProvider: prompt user to select their wallet
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+// ConnectionProvider: allows us to talk to nodes on the Solana blockchain via RPC
+// WalletProvider: an interface to connect different wallets
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import {
+  GlowWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
 import "../styles/App.css";
 
 const App = ({ Component, pageProps }) => {
 
+  // choices: [Devnet, Testnet, Mainnet]
+  // generate an RPC endpoint based on the network provided
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+    ],
+    [network]
+  );
+
   return (
-    <Component {...pageProps} />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps}/>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import HeadComponent from '../components/Head';
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
@@ -7,18 +10,31 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   
-  
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/Vgk2wun85lpVD59RbM/giphy.gif" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
+
   return (
     <div className="App">
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header">✨ Magical Fairy Dust Store 🦄</p>
+          <p className="sub-text">We accept magical internet money for magical virtual products.</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
